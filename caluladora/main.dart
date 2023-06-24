7cec9722be0f37f658042f9d64f6b312a7f01cdf
igor <igorsouza62092gmail.com>
2023-06-23 23:59:19 -0400

    calculadora

---
+++ caluladora/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Center(
+        child: Text('Calculator UI goes here'),
+      ),
+    );
+  }
+}
